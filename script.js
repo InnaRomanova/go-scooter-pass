795cd360404ef08a96a242969a5ca4eb8b4e931b
unknown <romanovainnap@yandex.ru>
2023-09-03 20:48:47 +0300

    slider block or grid

--- script.js
+++ script.js
@@ -1,13 +1,80 @@
 //Слайдер
-const sliders = document.querySelectorAll(".access__image");
 const slider = document.querySelector(".access__image-block");
-const buttonRight = document.querySelector(".access__rigth");
-const buttonLeft = document.querySelector(".access__left");
-// const dotsContainer = document.querySelector(".dots");
+const sliders = document.querySelectorAll(".access__image_icon");
+const nextButton = document.querySelector(".access__rigth");
+const prevButton = document.querySelector(".access__left");
 
-let currentSlide = 0;
-const maxSlides = sliders.length;
+const slides = Array.from(slider.querySelectorAll(".access__image"));
+const slideCount = sliders.length;
+let slideIndex = 0;
 
-//в начале нужны были, чтобы видеть.
-slider.style.scale = 0.5;
-slider.style.overflow = "visible";
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
+
+// // Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+// function updateSlider() {
+//   if (window.innerWidth <= 1033) {
+//     sliders.forEach((slide, index) => {
+//       if (index === slideIndex) {
+//         slide.style.display = "block";
+//       } else {
+//         slide.style.display = "none";
+//       }
+//     });
+//   }
+// }
+function updateSlider() {
+  if (window.innerWidth > 1033) {
+    // показываем все слайды
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "flex";
+    }
+  } else {
+    // показываем только текущий слайд
+    for (let i = 0; i < slides.length; i++) {
+      if (i === slideIndex) {
+        slides[i].style.display = "block";
+      } else {
+        slides[i].style.display = "none";
+      }
+    }
+  }
+}
+// function updateSlider() {
+//   if (window.innerWidth >= 1033) {
+//     // показываем все слайды
+//     slides.style.display = "flex";
+//   } else {
+//     // показываем только текущий слайд
+//     sliders.forEach((slide, index) => {
+//       if (index === slideIndex) {
+//         slide.style.display = "block";
+//       } else {
+//         slide.style.display = "none";
+//       }
+//     });
+// for (let i = 0; i < slides.length; i++) {
+//   if (i === slideIndex) {
+//     slides[i].style.display = "block";
+//   } else {
+//     slides[i].style.display = "none";
+//   }
+// }
+//   }
+// }
+
+// Инициализация слайдера
+updateSlider();
